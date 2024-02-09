deca76ae4dbdeaa74f7b106db84d59eb9df0084f
Devjeet Biswas <104926391+Devjeet-biswas@users.noreply.github.com>
2024-02-09 17:41:28 +0530

    Add files via upload

---
+++ valentine_project/script.js
@@ -0,0 +1,25 @@
+function respondToInvitation(accepted) {
+    if (accepted) {
+        window.location.href = 'questionnaire.html';
+    } else {
+        // You can handle different behavior if she declines
+        window.location.href = 'rejection.html';
+    }
+}
+
+
+function respondToInvitation2(accepted) {
+    if (accepted) {
+        window.location.href = 'questionnaire.html';
+    } else {
+        // You can handle different behavior if she declines
+
+            window.location.href = 'video.html';
+
+        //alert("Gandi ladki of the universe🥹👍🏼");
+    }
+}
+
+function holahola(){
+  window.location.href = 'video_2.html'
+}
